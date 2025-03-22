835fb4505c8d8790645b7730406efcf35fd71083
George Barlow <george@barlw.uk>
2025-03-22 15:52:07 +0000

    Makes more sense to have issues not docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
         },
         // { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/neoradar-project/docs",
+          href: "https://github.com/neoradar-project/issues/issues/new/choose",
           label: "GitHub",
           position: "right",
         },
