ec51522cd2daf973983fa747442c32b99b3acba4
George Barlow <george@barlw.uk>
2025-03-22 16:06:10 +0000

    Reword

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
         // { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/neoradar-project/issues/issues/new/choose",
-          label: "GitHub",
+          label: "Report an Issue",
           position: "right",
         },
       ],
